146309cd208c5a38e162ce7f51507d0b3a43b37a
JuanqQuijano <118690618+JuanqQuijano@users.noreply.github.com>
2025-07-09 17:40:58 -0500

    Create authMiddleware.js

---
+++ routes/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+// routes/middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Obtener token del header: 'Bearer TOKEN'
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verificar el token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Obtener el usuario del token y adjuntarlo al request
+      req.user = await User.findById(decoded.id).select('-password_hash');
+      
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: 'No autorizado, token falló' });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'No autorizado, no hay token' });
+  }
+};
+
+module.exports = { protect };
